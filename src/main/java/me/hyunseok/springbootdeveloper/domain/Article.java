aa9b1e90ed839f3ce6787fb927926ccbeb8fea1b
Yoo Hyunseok <brianyhs@naver.com>
2023-09-23 14:46:35 +0900

    fix: apply lombok to Article

--- src/main/java/me/hyunseok/springbootdeveloper/domain/Article.java
+++ src/main/java/me/hyunseok/springbootdeveloper/domain/Article.java
@@ -1,11 +1,15 @@
 package me.hyunseok.springbootdeveloper.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Article {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", updatable = false)
@@ -23,18 +27,4 @@ public class Article {
         this.content = content;
     }
 
-    protected Article() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getContent() {
-        return content;
-    }
 }
