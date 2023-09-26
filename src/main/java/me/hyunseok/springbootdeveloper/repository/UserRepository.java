1826a46f3208e244e51aa5b1c4f539a72129ab79
Yoo Hyunseok <brianyhs@naver.com>
2023-09-26 16:00:53 +0900

    feat: UserRepository findByEmail

---
+++ src/main/java/me/hyunseok/springbootdeveloper/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package me.hyunseok.springbootdeveloper.repository;
+
+import me.hyunseok.springbootdeveloper.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
+
+
