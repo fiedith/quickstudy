c6700c727616941cba4e274b3d54159be2496e85
Yoo Hyunseok <brianyhs@naver.com>
2023-09-26 17:42:24 +0900

    feat: 가입 관련 AddUserRequest, UserService using BCryptPasswordEncoder

---
+++ src/main/java/me/hyunseok/springbootdeveloper/service/UserService.java
@@ -0,0 +1,23 @@
+package me.hyunseok.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.hyunseok.springbootdeveloper.domain.User;
+import me.hyunseok.springbootdeveloper.dto.AddUserRequest;
+import me.hyunseok.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
